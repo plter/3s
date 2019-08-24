c98dde80c371201b1353977b0788488215a824f1
plter <xtiqin@163.com>
2019-08-24 18:18:30 +0800

    优化程序

--- SubProjects/FrontEnd/ViewHTML5MediaRecordDataAsStream/src/components/ViewHTML5MediaRecordDataAsStream.js
+++ SubProjects/FrontEnd/ViewHTML5MediaRecordDataAsStream/src/components/ViewHTML5MediaRecordDataAsStream.js
@@ -1,7 +1,7 @@
 import ViewHTML5MediaRecordDataAsStream from "../html/ViewHTML5MediaRecordDataAsStream.html"
-import MediaStreamPipeline from "../MediaStreamPipeline";
-import tools from "../tools";
+import ArrayBufferToVideoElement from "../pipelines/ArrayBufferToVideoElement";
 import Constants from "../Constants";
+import MediaStreamBlobToArrayBuffer from "../pipelines/MediaStreamBlobToArrayBuffer"
 
 Vue.component("viewhtml5mediarecorddataasstream", {
     template: ViewHTML5MediaRecordDataAsStream,
@@ -10,7 +10,8 @@ Vue.component("viewhtml5mediarecorddataasstream", {
     },
 
     mounted() {
-        this._pipeline = new MediaStreamPipeline(this.$refs.preview);
+        this._pipeline = new ArrayBufferToVideoElement(this.$refs.preview);
+        this._blobToArrayBufferPipeline = new MediaStreamBlobToArrayBuffer(this.gotBufferHandler.bind(this));
 
         this._asyncInit();
     },
@@ -24,12 +25,17 @@ Vue.component("viewhtml5mediarecorddataasstream", {
 
             let mr = this._currentMediaRecorder = new MediaRecorder(this._stream, {mimeType: Constants.MIME_TYPE});
             mr._isFirstBuffer = true;
-            mr.ondataavailable = async e => {
-                let buffer = await tools.blobToArrayBuffer(e.data);
-                this._pipeline.addBuffer(buffer, e.target._isFirstBuffer);
-                e.target._isFirstBuffer = false;
-            };
-            mr.start(150);
+            mr.ondataavailable = this.mediaRecorderDataAvailableHandler.bind(this);
+            mr.start(100);
+        },
+
+        mediaRecorderDataAvailableHandler(e) {
+            this._blobToArrayBufferPipeline.appendBlob(e.data, e.target._isFirstBuffer);
+            e.target._isFirstBuffer = false;
+        },
+
+        gotBufferHandler(arrayBuffer, isFirstPart) {
+            this._pipeline.addBuffer(arrayBuffer, isFirstPart);
         },
 
         async _asyncInit() {
