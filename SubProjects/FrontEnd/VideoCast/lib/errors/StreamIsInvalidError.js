ca6a083f13ada285cb071b10cf8b3fda01eaf4ae
plter <xtiqin@163.com>
2019-08-26 22:36:56 +0800

    Update StreamIsInvalidError.js

--- SubProjects/FrontEnd/VideoCast/lib/errors/StreamIsInvalidError.js
+++ SubProjects/FrontEnd/VideoCast/lib/errors/StreamIsInvalidError.js
@@ -1,6 +1,6 @@
 import SSSPlayerError from "./SSSPlayerError";
 
-export default class StreamIsInvalid extends SSSPlayerError {
+export default class StreamIsInvalidError extends SSSPlayerError {
 
     constructor() {
         super("Stream is invalid.");
