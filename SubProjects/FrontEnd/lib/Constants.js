bcbb05f29c9f42bf83a8a05c57309743fb7a4e58
peter <xtiqin@163.com>
2021-03-19 12:37:00 +0800

    Update Constants.js

--- SubProjects/FrontEnd/lib/Constants.js
+++ SubProjects/FrontEnd/lib/Constants.js
@@ -1,6 +1,6 @@
 export default {
     MIME_TYPE: 'video/webm; codecs="opus,vp8"',
-    MEDIA_RECORDER_TIME_SLICE: 50,//ms，单位是毫秒
+    MEDIA_RECORDER_TIME_SLICE: 30,//ms，单位是毫秒
     MAX_SKIP_TIME: 0.5//当播放器当前时间延后超过该值时，则直接跳至最新时间，以保持实时。单位是秒
 
 }
